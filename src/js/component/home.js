e6d10f8d67bf90cf524773b8de5c7a3cbfceb0d5
kedwards413 <kaela.rose.edwards@gmail.com>
2021-01-16 01:38:22 +0000

    API practice

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,88 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-}
+export const Home = () => {
+    const [list, setList] = useState([]);
+    const [userInput, setUserInput] = useState("");
+
+    useEffect(() => {
+        fetch("https://assets.breatheco.de/apis/fake/todos/user/kedwards413")
+            .then(function (response) {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                // Read the response as json.
+                return response.json();
+            })
+            .then(function (responseAsJson) {
+                setList(responseAsJson);
+            })
+            .catch(function (error) {
+                console.log("Looks like there was a problem: \n", error);
+            });
+    }, []);
+
+    const deleteItem = index => {
+        var updatedList = list.filter((task, taskIndex) => index != taskIndex);
+        setList(updatedList);
+    };
+
+    const handleKeyUp = () => {
+        if (event.keyCode == 13 && userInput != "") {
+            setList(list.concat(userInput));
+            setUserInput("");
+            fetch("https://assets.breatheco.de/apis/fake/todos/user/kedwards413", {
+                method: "PUT", // or 'POST'
+                body: JSON.stringify(list.concat(userInput)), // data can be `string` or {object}!
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw Error(response.statusText);
+                    }
+                    return response.json();
+                })
+				.then(response =>
+                    console.log("Success:", JSON.stringify(response)
+                    
+                    )
+                )
+                .catch(error => console.error("Error:", error));
+        }
+    };
+
+    return (
+        <div className="container">
+            <div className="text-center">
+                <h3>To Do List</h3>
+                <div className="toDoList">
+                    <input
+                        className="todoInput"
+                        onChange={event => setUserInput(event.target.value)}
+                        value={userInput}
+                        onKeyUp={handleKeyUp} // triggers the handleKeyUp function above when user hits "enter"
+                    />
+                    <ul className="list-group">
+                        {list.map((value, index) => {
+                            return (
+                                <ul>
+                                    <li className="list-group-item" key={index}>
+                                        <button
+                                            type="button"
+                                            className="btn btn-outline-primary"
+                                            onClick={() => deleteItem(index)}>
+                                            X
+										</button>
+                                        {value.label}
+                                    </li>
+                                </ul>
+                            );
+                        })}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
